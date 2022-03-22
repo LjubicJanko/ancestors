2e4f77b941a57a6ab2804873878d61249a18f635
LjubicJanko <39593701+LjubicJanko@users.noreply.github.com>
2022-03-22 19:22:44 +0000

    Deploying to gh-pages from @ LjubicJanko/ancestors@4f7d48acae5e7d144d50e3d2973bd0f6f13d6477 🚀

--- _next/static/xqDUWjPR3Arp40uCQSfMp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5ebb03332d2e5d7b.css","static/chunks/pages/index-06f76a369a766053.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
